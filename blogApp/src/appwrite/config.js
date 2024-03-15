c03b57162b76a0bc6d78348bd92194337250189e
Deepak Kumar <deepkr1099@gmail.com>
2024-03-15 12:53:52 +0000

    #

--- blogApp/src/appwrite/config.js
+++ blogApp/src/appwrite/config.js
@@ -15,6 +15,61 @@ export class Service{
         this.bucket =new Storage(this.client)
     }
 
+    async createPost({title,slug,content,featureImage,status,userID}){
+        try {
+            return await this.databases.createDocument(conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {title,content,featureImage,status,userID})
+        } catch (error) {
+            console.log("appwrite service :: createPost :: error",error);
+        }
+    }
+
+    async updatePost(slug,{title,content,featureImage,status}){
+        try{
+            return await this.databases.updateDocument(conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {title,content,featureImage,status}
+                )
+        }catch(error){
+            console.log("appwrite service:: updatePost :: error", error);
+        }
+    }
+
+    async deletePost(slug){
+        try{
+            await this.databases.deleteDocument(conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug)
+
+                return true;
+        }catch(error){
+            console.log("appwrite service :: deletePost :: error", error);
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return    await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )       
+        } catch (error) {
+            console.log("appwrite services :: getPost :: error", error  );
+        }
+    }
+
+    async getPosts(quaries=[Query.equal("status","active")]){
+        try {
+            
+        } catch (error) {
+            log
+        }
+    }
 }
 
 const service = new Service();
